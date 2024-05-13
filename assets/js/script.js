059ad3a2b70cfd99a953c078a3c3446435772df1
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-05-13 09:23:49 +0000

    Add basic structure and eventlisteners to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 
 
 
+    const currentQuestion=document.getElementById('current-question');
+    const questions=document.getElementById('totalQuestions');
+    const startScreen=document.getElementById(start);
+    const quizScreen=document.getElementById('quiz-screen');
+    const x=0;
+    const resultScreen=document.getElementById('result-screen');
+    const timeLeft=document.querySelector('time-left');
+    const userScore=document.getElementById('score');
+    const question=document.getElementById('questions');
+    const opt1=document.getElementById('opt1');
+    const opt2=document.getElementById('opt2');
+    const opt3=document.getElementById('opt3');
+    const opt4=document.getElementById('opt4');
+
+    
+
+
+
